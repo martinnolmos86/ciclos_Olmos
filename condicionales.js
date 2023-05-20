533a587560a054f5c6c4db89abe1b7280f1630ba
Martin Olmos <martinnolmos@gmail.com>
2023-05-20 17:11:43 -0300

    first commit

---
+++ condicionales.js
@@ -0,0 +1,56 @@
+// let number = 4;
+
+// if (number >= 0) {
+//   console.log(`El ${number} es positivo`);
+// } else {
+//   console.log(`${number} es negativo`);
+// }
+
+// console.log(number);
+
+// Crea un bucle While, este bucle tendrá que tener como condición que la variable numeroWhile sea inferior a 3,
+// el bloque de código que tendrá el bucle deberá:
+
+// Incrementar el valor de la variable en uno cada vez que se ejecute.
+
+// Mostrarlo por pantalla cada vez que se ejecute.
+
+// let numeroWhile = 0;
+// while (numeroWhile < 3) {
+//   console.log(numeroWhile);
+//   numeroWhile++;
+// }
+
+// Para el bucle Do While, deberás crear la misma estructura que en el While, pero solo se debe ejecutar una vez.
+
+// let numeroWhile = 0;
+// do {
+//   console.log(numeroWhile);
+// } while (numeroWhile < 3);
+
+// Para el bucle For, crea una variable numeroFor, esta variable tendrá como valor 0 y su condición será que la variable sea igual o menor que 3, se irá incrementando en 1
+// su valor cada vez que se ejecute y deberá mostrarse por pantalla.
+// let numeroFor = 2;
+// for (let index = 0; index <= 3; index++) {
+//   console.log(index);
+// }
+
+// Por último, para el Switch, deberás crear la variable estacion, y distintos case para las cuatro estaciones del año. Dependiendo del
+// valor de la variable estacion se deberá mandar un mensaje por consola informando de la estación en la que está. También habrá que poner
+// un default para cuando el valor de la variable no sea una estación.
+
+// let estacion = "Verano";
+// switch (estacion) {
+//   case "Otono":
+//     console.log("Estamos en Otoño");
+//     break;
+//   case "Invierno":
+//     console.log("Estamos en Invierno");
+//     break;
+//   case "Primavera":
+//     console.log("Estamos en Primavera");
+//     break;
+//   default:
+//     console.log("Estamos en Verano");
+//     break;
+// }
--- java.java
+++ java.java
@@ -1,38 +1,84 @@
-
-// primero
+// Usando un if, crear una condición que compare si la variable numeroIf es positivo, negativo, o 0.
+// Pista: Los números inferiores a 0 son negativos y los superiores, positivos.
 
 public class Main {
-    public static int sumar(int num1, int num2, int num3) {
-        return num1 + num2 + num3;
-    }
-
     public static void main(String[] args) {
-        int resultado = sumar(2, 4, 6);
-        System.out.println("El resultado de la suma es: " + resultado);
+        int number = 4;
+        
+        if (number >= 0) {
+            System.out.println("El " + number + " es positivo");
+        } else {
+            System.out.println(number + " es negativo");
+        }
     }
 }
 
 
-// segundo
 
-public class Coche {
-    private int numPuertas;
+// Crea un bucle While, este bucle tendrá que tener como condición que la variable numeroWhile sea inferior a 3,
+// el bloque de código que tendrá el bucle deberá:
 
-    public Coche() {
-        numPuertas = 0;
-    }
+// Incrementar el valor de la variable en uno cada vez que se ejecute.
 
-    public void incrementarPuertas() {
-        numPuertas++;
-    }
+// Mostrarlo por pantalla cada vez que se ejecute.
 
-    public int getNumPuertas() {
-        return numPuertas;
-    }
 
+
+public class Main {
     public static void main(String[] args) {
-        Coche miCoche = new Coche();
-        miCoche.incrementarPuertas();
-        System.out.println("Número de puertas: " + miCoche.getNumPuertas());
+        int numeroWhile = 0;
+        while (numeroWhile < 3) {
+            System.out.println(numeroWhile);
+            numeroWhile++;
+        }
+    }
+}
+
+// Para el bucle Do While, deberás crear la misma estructura que en el While, pero solo se debe ejecutar una vez.
+
+public class Main {
+    public static void main(String[] args) {
+        int numeroDoWhile = 0;
+        do {
+            System.out.println(numeroDoWhile);
+            numeroDoWhile++;
+        } while (numeroDoWhile < 3);
+    }
+}
+
+// Para el bucle For, crea una variable numeroFor, esta variable tendrá como valor 0 y su condición será que la variable sea igual o menor que 3, se irá incrementando en 1
+// su valor cada vez que se ejecute y deberá mostrarse por pantalla.
+
+public class Main {
+    public static void main(String[] args) {
+        int numeroFor = 2;
+        for (let index = 0; index <= 3; index++) {
+   System.out.println(numeroFor);
+}
+
+    }
+}
+
+// / Por último, para el Switch, deberás crear la variable estacion, y distintos case para las cuatro estaciones del año. Dependiendo del
+// valor de la variable estacion se deberá mandar un mensaje por consola informando de la estación en la que está. También habrá que poner
+// un default para cuando el valor de la variable no sea una estación.
+
+public class Main {
+    public static void main(String[] args) {
+        String estacion = "Verano";
+        switch (estacion) {
+            case "Otono":
+                System.out.println("Estamos en Otoño");
+                break;
+            case "Invierno":
+                System.out.println("Estamos en Invierno");
+                break;
+            case "Primavera":
+                System.out.println("Estamos en Primavera");
+                break;
+            default:
+                System.out.println("Estamos en Verano");
+                break;
+        }
     }
 }
